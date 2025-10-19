f336037176d2cc69f4d0e1f349cd057cb2bc2f9a
nishvaPatel <nishva3182006@gmail.com>
2025-10-19 14:54:03 +0530

    From Nishva

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { userAPI } from '../services/api';
-import '../styles/Auth.css';
+import '../styles/LoginRegister.css';
 
 const Login = ({ onUserLogin }) => {
   const [formData, setFormData] = useState({
